dd1cffb16ff7bbe4811f8d024b176d2dd262f5d9
Enes Duman <ensdman26@gmail.com>
2024-10-16 16:39:22 +0300

    Created a script with its folder.

---
+++ HW1/Assets/Scripts/MoveableExample.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Movable object script started!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
